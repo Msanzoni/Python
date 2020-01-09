c45bf4515813bc4b617ca6d4fc775dcab3da1714
Msanzoni <35820532+Msanzoni@users.noreply.github.com>
2020-01-08 23:06:14 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,10 @@
+nome=input("Qual seu nome?")
+dia=input("Dia do nascimento?")
+mes=input("Mẽs de nascimento?")
+ano=input("Ano de nascimento?")
+msg1=("Você nasceu em")
+msg2=("Correto?")
+print(nome, msg1, dia, 'de',mes, 'de' ,ano, msg2)
+
+
+print(nome,dia, mes)
---
+++ script_soma.py
@@ -0,0 +1,4 @@
+v1=input("Valor um?")
+v2=input("Valor dois?")
+print(int(v1) +int(v2))
+
